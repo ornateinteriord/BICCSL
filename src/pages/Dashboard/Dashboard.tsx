7448d590c0fd954724b3ef47d0b74455883008fa
spexzee <abub5979@gmail.com>
2025-01-11 13:16:03 +0530

    some updates on navbar and sidebar

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,43 +1,20 @@
+import {  BoxesCore } from '../../components/ui/background-boxes';
+import { cn } from '../../lib/utils';
 import './dashboard.scss';
-import { Box, Typography, Container } from '@mui/material';
 
 const Dashboard = () => {
   return (
-    <div className="dashboard">
-      <Box
-        sx={{
-          background: 'linear-gradient(150deg, #283159 30%, #9d75add1 90%);',
-          py: 2, 
-          boxShadow: '0 3px 5px 2px #23303dc9',
-        }}
-      >
-        <Container>
-          <Typography
-            variant="h3"
-            component="h1"
-            sx={{
-              color: 'white',
-              fontWeight: 'bold',
-              textAlign: 'center',
-              textShadow: '2px 2px 4px rgba(0,0,0,0.3)'
-            }}
-          >
-            Welcome to MLM Dashboard
-          </Typography>
-          <Typography
-            variant="h6"
-            sx={{
-              color: 'white',
-              textAlign: 'center',
-              mt: 2,
-              opacity: 0.9
-            }}
-          >
-            Manage your network and track your success
-          </Typography>
-        </Container>
-      </Box>
-    </div>
+    <div className="h-40 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
+    <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+
+    <BoxesCore />
+    <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
+      Welcome to MLM Dashboard
+    </h1>
+    <p className="text-center mt-2 text-neutral-300 relative z-20">
+      Manage your network and track your success
+    </p>
+  </div>
   )
 }
 
--- src/pages/Navbar/Navbar.tsx
+++ src/pages/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { AppBar, Toolbar, Typography } from '@mui/material';
 const Navbar = () => {
   return (
     <>
-      <AppBar position="static" className="navbar" style={{ backgroundColor: '#313e4b' }}>
+      <AppBar position="static" className="navbar" style={{ background: 'linear-gradient(170deg, #0f172a 43%, #071337 70%)' }}>
         <Toolbar className="navbar-toolbar">
           <Typography variant="h4" className="navbar-title" style={{ marginLeft: '12px' }}>
             BICCSL
