713d9cabd18b453580a9db967890474447307571
sid-1974 <sidgithub1974@gmail.com>
2025-02-11 22:13:00 +0530

    Error Toaster added

--- src/pages/User-Pages/Transaction/Transaction.tsx
+++ src/pages/User-Pages/Transaction/Transaction.tsx
@@ -1,9 +1,21 @@
-import DataTable from 'react-data-table-component';
-import { Card, CardContent, Accordion, AccordionSummary, AccordionDetails, TextField, CircularProgress } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { DASHBOARD_CUTSOM_STYLE, getTransactionColumns } from '../../../utils/DataTableColumnsProvider';
-import { useGetTransactionDetails } from '../../../api/Memeber';
-import { useEffect } from 'react';
+import DataTable from "react-data-table-component";
+import {
+  Card,
+  CardContent,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  TextField,
+  CircularProgress,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  DASHBOARD_CUTSOM_STYLE,
+  getTransactionColumns,
+} from "../../../utils/DataTableColumnsProvider";
+import { useGetTransactionDetails } from "../../../api/Memeber";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 interface Transaction {
   transaction_date: string;
@@ -12,38 +24,47 @@ interface Transaction {
   ew_debit: number;
   status: string;
 }
-
 const Transaction = () => {
-  const userId = localStorage.getItem('userId')
-  const { data: transactions, isLoading, isError, error } = useGetTransactionDetails(userId!)
+  const userId = localStorage.getItem("userId");
+  const {
+    data: transactions,
+    isLoading,
+    isError,
+    error,
+  } = useGetTransactionDetails(userId!);
 
   useEffect(() => {
     if (isError) {
-      console.error(error?.message || 'Failed to fetch Transaction details')
+      const err = error as any;
+
+      toast.error(
+        err?.response.data.message || "Failed to fetch Transaction details"
+      );
     }
-  }, [isError, error])
+  }, [isError, error]);
+  const data =
+    transactions?.map((transaction: Transaction) => ({
+      date: transaction.transaction_date || "-",
+      description: transaction.description || "-",
+      credits: transaction.ew_credit || "-",
+      debit: transaction.ew_debit || "-",
+      status: transaction.status || "-",
+    })) || [];
 
-  const data = transactions?.map((transaction: Transaction) => ({
-    date: transaction.transaction_date || "-",
-    description: transaction.description || "-",
-    credits: transaction.ew_credit || "-",
-    debit: transaction.ew_debit || "-",
-    status: transaction.status || "-",
-  })) || [];
-
-
-  const noDataComponent = <div style={{ padding: '24px' }}>No data available in table</div>;
+  const noDataComponent = (
+    <div style={{ padding: "24px" }}>No data available in table</div>
+  );
 
   return (
-    <Card sx={{ margin: '2rem', mt: 10 }}>
+    <Card sx={{ margin: "2rem", mt: 10 }}>
       <CardContent>
         <Accordion defaultExpanded>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             sx={{
-              backgroundColor: '#04112f',
-              color: '#fff',
-              '& .MuiSvgIcon-root': { color: '#fff' }
+              backgroundColor: "#04112f",
+              color: "#fff",
+              "& .MuiSvgIcon-root": { color: "#fff" },
             }}
           >
             List of Transaction
@@ -56,13 +77,22 @@ const Transaction = () => {
               customStyles={DASHBOARD_CUTSOM_STYLE}
               paginationPerPage={25}
               paginationRowsPerPageOptions={[25, 50, 100]}
-              highlightOnHover  
+              highlightOnHover
               progressPending={isLoading}
-              progressComponent={<CircularProgress size={'4rem'} sx={{color:'#04112F'}}/>}
+              progressComponent={
+                <CircularProgress size={"4rem"} sx={{ color: "#04112F" }} />
+              }
               noDataComponent={noDataComponent}
               subHeader
               subHeaderComponent={
-                <div style={{ display: 'flex', justifyContent: 'flex-end', width: '100%', padding: '0.5rem' }}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    width: "100%",
+                    padding: "0.5rem",
+                  }}
+                >
                   <TextField
                     placeholder="Search"
                     variant="outlined"
@@ -75,7 +105,6 @@ const Transaction = () => {
         </Accordion>
       </CardContent>
     </Card>
-
   );
 };
 
